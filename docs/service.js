5251c86cd0d4a9f054c758a16c4a5acea2b318a9
Lat Murmeldjur <lat.murmeldjur@gmail.com>
2025-01-12 18:45:19 +0300

    "Commit number 99"

--- docs/service.js
+++ docs/service.js
@@ -5,7 +5,7 @@ const putInCache = async (request, response) => {
 };
 
 self.addEventListener('message', async function(event) {
-  let asset0 = new Blob([event.data.data0], {type: event.data.mime0});
+  let asset0 = new Blob([event.data.data0[0]], {type: event.data.mime0});
 
   const reqHeaders = new Headers();
   reqHeaders.set("Cache-Control", "public, max-age=60000000000000");
@@ -14,7 +14,7 @@ self.addEventListener('message', async function(event) {
   };
 
   const request0 = new Request(event.data.path0, options);
-  const response0 = new Response(asset0, { headers: { 'Content-Type': event.data.mime0, 'Content-Length': event.data.data0.length } });
+  const response0 = new Response(asset0, { headers: { 'Content-Type': event.data.mime0, 'Content-Length': event.data.data0[0].length } });
 
   await putInCache(request0, response0);
   console.log(event);
